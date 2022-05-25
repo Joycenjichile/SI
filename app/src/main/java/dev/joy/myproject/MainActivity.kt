618f3e34b5f129c41a62bd7f39dccbf60c7d51a8
joycenjichile <joycenkimalantoi@gmail.com>
2022-05-25 14:59:27 +0300

    first commit

---
+++ app/src/main/java/dev/joy/myproject/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.joy.myproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnSignup:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
